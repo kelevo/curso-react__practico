9ba9065abe555f3674bbbbd9c5cbc9d5728e7c8c
kelevo <patrickjhonatanh@gmail.com>
2021-05-17 18:50:16 -0500

    Actualizacion del proyecto con React Router

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useInitialState = (API) => {
-    const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
-
+    const [videos, setVideos] = useState([]);
     useEffect(() => {
         fetch(API)
             .then(response => response.json())
